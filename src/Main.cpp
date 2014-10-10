1bf2da3e6500eec088c7d6e70d4f0fe31e34515e
Tyler Veness <calcmogul@gmail.com>
2014-10-09 21:57:00 -0700

    client: enabled OpenGL lighting

--- src/Main.cpp
+++ src/Main.cpp
@@ -102,7 +102,21 @@ int main() {
     glEnable( GL_ALPHA_TEST );
     glDisable( GL_TEXTURE_2D );
     glBlendFunc( GL_SRC_ALPHA , GL_ONE_MINUS_SRC_ALPHA );
-    glShadeModel( GL_FLAT );
+
+    GLfloat mat_specular[] = { 1.0 , 1.0 , 1.0 , 1.0 };
+    GLfloat mat_shininess[] = { 50.0 };
+    GLfloat light_position[] = { 1.0 , 1.0 , 1.0 , 0.0 };
+    GLfloat light_ambient[] = { 0.5 , 0.5 , 0.5 , 1.0 };
+    glShadeModel( GL_SMOOTH );
+
+    glMaterialfv( GL_FRONT , GL_SPECULAR , mat_specular );
+    glMaterialfv( GL_FRONT , GL_SHININESS , mat_shininess );
+    glLightfv( GL_LIGHT0 , GL_POSITION , light_position );
+    glLightfv( GL_LIGHT0 , GL_AMBIENT , light_ambient );
+
+    glEnable( GL_LIGHTING );
+    glEnable( GL_LIGHT0 );
+    glEnable( GL_COLOR_MATERIAL );
 
     // Set up screen
     glViewport( 0 , 0 , mainWin.getSize().x , mainWin.getSize().y );
