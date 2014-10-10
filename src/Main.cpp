13d9f22dbfde4161b4cdb60090e4193f5bb538ac
Tyler Veness <calcmogul@gmail.com>
2014-10-09 22:17:55 -0700

    client: made glColor() calls control ambient light

--- src/Main.cpp
+++ src/Main.cpp
@@ -106,17 +106,16 @@ int main() {
     GLfloat mat_specular[] = { 1.0 , 1.0 , 1.0 , 1.0 };
     GLfloat mat_shininess[] = { 50.0 };
     GLfloat light_position[] = { 1.0 , 1.0 , 1.0 , 0.0 };
-    GLfloat light_ambient[] = { 0.5 , 0.5 , 0.5 , 1.0 };
     glShadeModel( GL_SMOOTH );
 
     glMaterialfv( GL_FRONT , GL_SPECULAR , mat_specular );
     glMaterialfv( GL_FRONT , GL_SHININESS , mat_shininess );
     glLightfv( GL_LIGHT0 , GL_POSITION , light_position );
-    glLightfv( GL_LIGHT0 , GL_AMBIENT , light_ambient );
 
     glEnable( GL_LIGHTING );
     glEnable( GL_LIGHT0 );
     glEnable( GL_COLOR_MATERIAL );
+    glColorMaterial( GL_FRONT , GL_AMBIENT_AND_DIFFUSE );
 
     // Set up screen
     glViewport( 0 , 0 , mainWin.getSize().x , mainWin.getSize().y );
