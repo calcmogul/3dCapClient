8603e5acf0a3e1540539dab99205b6517c4e651d
Tyler Veness <calcmogul@gmail.com>
2025-12-16 15:03:30 -0800

    Avoid virtual function call in base class

--- src/FilterBase.hpp
+++ src/FilterBase.hpp
@@ -7,21 +7,29 @@
 /// Provides an interface for filter classes
 class FilterBase {
 public:
-    FilterBase();
+    FilterBase() = default;
     virtual ~FilterBase() = default;
 
     virtual void update(double input) = 0;
-    virtual void reset();
+
+    void reset() {
+        using namespace std::chrono_literals;
+
+        m_stateEstimate = 0.0;
+        m_dt = 0.0s;
+        m_lastTime = std::chrono::system_clock::now();
+    }
 
     /// Return the filtered value
-    double getEstimate();
+    double getEstimate() { return m_stateEstimate; }
 
 protected:
-    double m_stateEstimate;
+    double m_stateEstimate = 0.0;
 
     /// Holds dt in update()
-    std::chrono::duration<double> m_dt;
+    std::chrono::duration<double> m_dt{0.0};
 
     /// Used to find dt in update()
-    std::chrono::time_point<std::chrono::system_clock> m_lastTime;
+    std::chrono::time_point<std::chrono::system_clock> m_lastTime{
+        std::chrono::system_clock::now()};
 };
