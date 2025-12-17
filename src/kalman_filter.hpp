820610bbf428bedf2a1c003415b9c0f40a2dfbeb
Tyler Veness <calcmogul@gmail.com>
2025-12-16 18:36:34 -0800

    Clean up filters

--- src/kalman_filter.hpp
+++ src/kalman_filter.hpp
@@ -2,25 +2,64 @@
 
 #pragma once
 
-#include "filter_base.hpp"
-
-// m_stateEstimate == xHat, the running estimate of state calculated by filter
+#include <chrono>
+#include <cmath>
 
 /// Implements a Kalman filter for single input filtering
-class KalmanFilter : public FilterBase {
+class KalmanFilter {
 public:
-    KalmanFilter(double Q, double R);
+    KalmanFilter(double Q, double R) : m_Q{Q}, m_R{R} {}
 
-    void update(double input);
+    void update(double input) {
+        // Get the current dt since the last call to update()
+        m_dt = std::chrono::system_clock::now() - m_last_time;
 
-    void set_Q(double Q);
-    void set_R(double R);
+        // Initialize estimate to measured value
+        if (m_first_run) {
+            m_x = input;
 
-    void reset();
+            m_first_run = false;
+        }
+
+        // Project the error covariance ahead
+        m_P += m_Q * m_dt.count();
+
+        // Calculate the Kalman gain
+        double K = m_P / (m_P + m_R);
+
+        // Correct state estimate
+        m_x += K * (input - m_x);
+        m_P -= K * m_P;
+
+        if (std::isnan(m_x)) {
+            m_x = 0.0;
+        }
+
+        // Update the previous time for the next delta
+        m_last_time = std::chrono::system_clock::now();
+    }
+
+    /// Return the filtered value
+    constexpr double get_estimate() { return m_x; }
+
+    void reset() {
+        using namespace std::chrono_literals;
+
+        m_x = 0.0;
+        m_P = 0.0;
+
+        m_dt = 0s;
+        m_last_time = std::chrono::system_clock::now();
+
+        m_first_run = true;
+    }
 
 private:
+    // State estimate
+    double m_x = 0.0;
+
     // Covariance of error
-    double m_P;
+    double m_P = 0.0;
 
     // Process noise variance
     double m_Q;
@@ -28,6 +67,13 @@ private:
     // Measurement noise variance
     double m_R;
 
+    /// Holds dt in update()
+    std::chrono::duration<double> m_dt{0.0};
+
+    /// Used to find dt in update()
+    std::chrono::time_point<std::chrono::system_clock> m_last_time{
+        std::chrono::system_clock::now()};
+
     // If true, update() sets current estimate to first measurement
-    bool m_first_run;
+    bool m_first_run = true;
 };
--- src/weighted_average_filter.hpp
+++ src/weighted_average_filter.hpp
@@ -2,17 +2,51 @@
 
 #pragma once
 
-#include "filter_base.hpp"
-
-// m_stateEstimate == the average state
+#include <chrono>
+#include <cmath>
 
 /// Averages new value with old one using a given weight for the new one [0..1]
-class WeightedAverageFilter : public FilterBase {
+class WeightedAverageFilter {
 public:
-    explicit WeightedAverageFilter(float adapt);
+    explicit WeightedAverageFilter(float alpha) : m_alpha{alpha} {}
 
-    void update(double input);
+    void update(double input) {
+        if (std::isnan(input)) {
+            return;
+        }
+
+        // Get the current dt since the last call to update()
+        m_dt = std::chrono::system_clock::now() - m_last_time;
+
+        m_x =
+            m_x * (1 - m_alpha * m_dt.count()) + input * m_alpha * m_dt.count();
+
+        // Update the previous time for the next delta
+        m_last_time = std::chrono::system_clock::now();
+    }
+
+    /// Return the filtered value
+    constexpr double get_estimate() { return m_x; }
+
+    void reset() {
+        using namespace std::chrono_literals;
+
+        m_x = 0.0;
+
+        m_dt = 0s;
+        m_last_time = std::chrono::system_clock::now();
+    }
 
 private:
-    float m_adapt;
+    // State estimate
+    double m_x = 0.0;
+
+    float m_alpha;
+
+    /// Holds dt in update()
+    std::chrono::duration<double> m_dt{0.0};
+
+    /// Used to find dt in update()
+    std::chrono::time_point<std::chrono::system_clock> m_last_time{
+        std::chrono::system_clock::now()};
 };
